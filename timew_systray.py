edc219b95da833095be4fa30667086094f78ffe9
Akira - Cleber Akira Nakandakare <cakira@cpqd.com.br>
2025-02-17 14:20:51 -0300

    First commit, barely functional

---
+++ timew_systray.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# Copyright Cleber Akira Nakandakare
+# SPDX-License-Identifier: MIT
+
+# Requires infi.systray. Install it with `pip install infi.systray`
+# Monitors a file and creates a systray icon for Timewarrior.
+
+from pathlib import Path
+from time import sleep
+from threading import Event
+from infi.systray import SysTrayIcon
+
+MONITORED_FILE = Path(r"C:\Users\cakira\.local\state\timewarrior\current_tags.txt")
+ICON_FOLDER = Path("icons")
+ICON_DEFAULT = ICON_FOLDER / "timewarrior.ico"
+ICON_EMPTY = ICON_FOLDER / "timewarrior_empty.ico"
+
+
+class TimewarriorSystray:
+    def __init__(self):
+        self.stop_event = Event()
+        self.systray_icon = SysTrayIcon(
+            str(ICON_DEFAULT), "uninitialized", on_quit=self._on_quit_callback
+        )
+
+    def _on_quit_callback(self, _systray):
+        """Handles systray exit."""
+        self.stop_event.set()
+
+    def monitor_file(self):
+        """Monitors the file and updates the systray icon."""
+        last_state = None
+
+        with self.systray_icon:
+            while not self.stop_event.is_set():
+                try:
+                    with MONITORED_FILE.open() as current_tags_file:
+                        tags = current_tags_file.readline().strip()
+                except FileNotFoundError:
+                    tags, icon = "None", ICON_EMPTY
+                else:
+                    icon = ICON_DEFAULT if tags else ICON_EMPTY
+                    tags = tags or "None"
+
+                new_state = (icon, tags)
+                if new_state != last_state:  # Update only if changed
+                    self.systray_icon.update(str(icon), tags)
+                    last_state = new_state
+
+                sleep(5)
+
+
+if __name__ == "__main__":
+    app = TimewarriorSystray()
+    app.monitor_file()
